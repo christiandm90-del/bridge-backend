a83d11489d172db08411d831a34e5055ba94d7dd
Chris <282111132+christiandm90-del@users.noreply.github.com>
2026-05-08 20:25:48 +0200

    update

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const admin = require("firebase-admin");
 const cors = require("cors");
 
 // 🔐 Firebase APPBASE service account
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 // 🔥 Init Firebase Admin
 admin.initializeApp({
